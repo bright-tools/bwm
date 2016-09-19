8b1397e8e7077e3d9d5fa209ddb92a18133f4653
bright-tools <dev@brightsilence.com>
2016-09-20 00:16:03 +0100

    Initial implementation of app in C#

---
+++ src/bwm/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace bwm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : System.Windows.Application
+    {
+        public App() : base()
+        {
+            ShutdownMode = ShutdownMode.OnMainWindowClose;
+        }
+    }
+}
